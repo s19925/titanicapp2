32a09d30eaea3ea61f67d879d3aa68d2282cd5e5
S19925 <80154425+s19925@users.noreply.github.com>
2023-05-11 21:16:45 +0200

    Add files via upload

--- app_broken2.py
+++ app_broken2.py
@@ -1,68 +1,53 @@
+# źródło danych [https://www.kaggle.com/c/titanic/](https://www.kaggle.com/c/titanic)
+
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 import pickle
+from datetime import datetime
 
-def fill_empty_rows_with_medians(data):
-    for column in data.columns:
-        if data[column].dtype in [np.int64, np.float64]:  # Check if column is numeric
-            column_median = data[column].fillna(data[column].median())
-            data[column] = column_median
-    return data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-def train_model(data):
-    # Extract the features and target variable
-    features = data[['objawy', 'wiek', 'choroby_wsp', 'wzrost', 'leki']]
-    target = data['zdrowie']
+startTime = datetime.now()
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+# import znanych nam bibliotek
 
-    # Train the model
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
+import pathlib
+from pathlib import Path
 
-    return model
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+data = pd.read_csv("DSP_13.csv")
+# Extract the features and target variable
+features = data[['objawy', 'wiek', 'choroby_wsp', 'wzrost', 'leki']]
+target = data['zdrowie']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+
+# Train the model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
 
 def main():
 
-    # Read the CSV file
-    data = pd.read_csv("DSP_13.csv")
+	st.set_page_config(page_title="Titanic App")
+	overview = st.container()
+	left, right = st.columns(2)
+	prediction = st.container()
 
-    # Fill empty rows with column medians
-    data_filled = fill_empty_rows_with_medians(data)
+	st.image("https://upload.wikimedia.org/wikipedia/commons/f/fd/RMS_Titanic_3.jpg")
 
-    # Train the model
-    model = train_model(data_filled)
+	with overview:
+		st.title("Titanic App")
 
-    st.set_page_config(page_title="Titanic App")
-    overview = st.container()
-    left, right = st.columns(2)
-    prediction = st.container()
+	survival = model.predict(data)
+	s_confidence = model.predict_proba(data)
 
-    st.image("https://upload.wikimedia.org/wikipedia/commons/f/fd/RMS_Titanic_3.jpg")
-
-    with overview:
-        st.title("Titanic App")
-
-    with right:
-        age_slider = st.slider("wiek", value=1, min_value=1, max_value=90)
-        sibsp_slider = st.slider("choroby wsp", min_value=0, max_value=10)
-        parch_slider = st.slider("wzrost", min_value=1, max_value=300)
-        fare_slider = st.slider("objawy", min_value=1, max_value=5)
-
-
-    data = [[age_slider,sibsp_slider, parch_slider, fare_slider]]
-    survival = model.predict(data)
-    s_confidence = model.predict_proba(data)
-
-    with prediction:
-        st.subheader("Czy taka osoba przeżyłaby katastrofę?")
-        st.subheader(("Tak" if survival[0] == 1 else "Nie"))
-        st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
+	with prediction:
+		st.subheader("Czy taka osoba przeżyłaby katastrofę?")
+		st.subheader(("Tak" if survival[0] == 1 else "Nie"))
+		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 if __name__ == "__main__":
     main()
