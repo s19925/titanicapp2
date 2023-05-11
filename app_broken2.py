9ef1479cb20a34f746e9ebd358916cc6f5ed9f69
S19925 <80154425+s19925@users.noreply.github.com>
2023-05-11 21:29:05 +0200

    Add files via upload

--- app_broken2.py
+++ app_broken2.py
@@ -4,18 +4,30 @@ from datetime import datetime
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 
 startTime = datetime.now()
 
 # import znanych nam bibliotek
 
-data = pd.read_csv("DSP_13.csv")
+data = pd.read_csv("DSP_13.csv", delimiter=';')
+print(data.columns)  # Print the column names to check for discrepancies
 # Extract the features and target variable
 features = data[['objawy', 'wiek', 'choroby_wsp', 'wzrost', 'leki']]
 target = data['zdrowie']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+
+
+# Create an imputer object with strategy='median'
+imputer = SimpleImputer(strategy='median')
+
+# Fit the imputer on the training data and transform it
+X_train = imputer.fit_transform(X_train)
+
+# Transform the testing data using the fitted imputer
+X_test = imputer.transform(X_test)
 
 # Train the model
 model = RandomForestClassifier()
