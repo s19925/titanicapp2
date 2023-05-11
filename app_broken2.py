a21f678e80d48290e7c7a1936fe2980ae246e0d4
S19925 <80154425+s19925@users.noreply.github.com>
2023-05-11 21:18:01 +0200

    Add files via upload

--- app_broken2.py
+++ app_broken2.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import pickle
 from datetime import datetime
-
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
