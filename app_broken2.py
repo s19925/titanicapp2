8026824e78731d02d2f8bd844a89b24141f2c3ee
S19925 <80154425+s19925@users.noreply.github.com>
2023-05-11 21:51:09 +0200

    Add files via upload

--- app_broken2.py
+++ app_broken2.py
@@ -1,34 +1,42 @@
 import streamlit as st
 import pickle
 from datetime import datetime
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+startTime = datetime.now()
+import os
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import pd.fillna
 
+#uczenie modelu
+
+data = pd.read_csv("DSP_13.csv", sep=";")
+y = data.iloc[:,-1]
+data.drop(['objawy','wiek','choroby_wsp', 'wzrost', 'leki', 'zdrowie'], axis = 1, inplace = True)
+data(['objawy','wiek','choroby_wsp', 'wzrost', 'leki', 'zdrowie']).fillna(0)
+x = data.iloc[:,0:5]
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+model = RandomForestClassifier(n_estimators = 10, max_depth = 4, random_state=0)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+y_pred
 # import znanych nam bibliotek
 
+import pathlib
+from pathlib import Path
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# otwieramy wcześniej wytrenowany model
+
+sex_d ={0:"Mężczyzna",1:"Kobieta"}
+pclass_d = {0:"Pierwsza",1:"Druga", 2:"Trzecia"}
+embarked_d = {0:"Cherbourg", 1:"Queenstown", 2:"Southampton"}
+# o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
+
 def main():
-	data = pd.read_csv("DSP_13.csv", delimiter=';')
-	# Extract the features and target variable
-	features = data[['objawy', 'wiek', 'choroby_wsp', 'wzrost', 'leki']]
-	target = data['zdrowie']
-
-	# Split the data into training and testing sets
-	X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
-
-	# Create an imputer object with strategy='median'
-	imputer = SimpleImputer(strategy='median')
-
-	# Fit the imputer on the training data and transform it
-	X_train = imputer.fit_transform(X_train)
-
-	# Transform the testing data using the fitted imputer
-	X_test = imputer.transform(X_test)
-
-	# Train the model
-	model = RandomForestClassifier()
-	model.fit(X_train, y_train)
 
 	st.set_page_config(page_title="Titanic App")
 	overview = st.container()
@@ -40,29 +48,20 @@ def main():
 	with overview:
 		st.title("Titanic App")
 
+	with left:
+		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x])
+		pclass_radio = st.radio("Class",list(pclass_d.keys()),index = 2,format_func = lambda x:pclass_d[x])
+		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x])
+
 	with right:
-		objawy_slider = st.slider("Objawy", value=1, min_value=0, max_value=10)
-		wiek_slider = st.slider("Wiek", value=1, min_value=0, max_value=120)
-		choroby_wsp_slider = st.slider("Choroby współistniejące", value=1, min_value=0, max_value=5)
-		wzrost_slider = st.slider("Wzrost", value=1, min_value=120, max_value=220)
-		leki_slider = st.slider("Leki", value=1, min_value=0, max_value=10)
+		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=90)
+		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
+		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=10)
+		fare_slider = st.slider("Cena biletu", min_value=0, max_value=480, step=1)
 
-	medians = data.median()
-	# replace the missing values with the medians
-	objawy_slider = medians['objawy'] if objawy_slider is None else objawy_slider
-	wiek_slider = medians['wiek'] if wiek_slider is None else wiek_slider
-	choroby_wsp_slider = medians['choroby_wsp'] if choroby_wsp_slider is None else choroby_wsp_slider
-	wzrost_slider = medians['wzrost'] if wzrost_slider is None else wzrost_slider
-	leki_slider = medians['leki'] if leki_slider is None else leki_slider
-
-	data = [[objawy_slider, wiek_slider, choroby_wsp_slider, wzrost_slider, leki_slider]]
+	data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
 	survival = model.predict(data)
 	s_confidence = model.predict_proba(data)
-
-	with prediction:
-		st.subheader("Czy taka osoba przeżyłaby katastrofę?")
-		st.subheader(("Tak" if survival[0] == 1 else "Nie"))
-		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 	with prediction:
 		st.subheader("Czy taka osoba przeżyłaby katastrofę?")
