b4d0faee763f87dedd3f78c100e0bbba5cec67d2
S19925 <80154425+s19925@users.noreply.github.com>
2023-05-11 20:37:45 +0200

    Add files via upload

--- app_broken2.py
+++ app_broken2.py
@@ -48,18 +48,13 @@ def main():
 	with overview:
 		st.title("Titanic App")
 
-	with left:
-		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x])
-		pclass_radio = st.radio("Class",list(pclass_d.keys()),index = 2,format_func = lambda x:pclass_d[x])
-		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x])
-
 	with right:
 		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=90)
 		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
 		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=10)
 		fare_slider = st.slider("Cena biletu", min_value=0, max_value=480, step=1)
 
-	data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+	data = [[age_slider, sibsp_slider, parch_slider, fare_slider]]
 	survival = model.predict(data)
 	s_confidence = model.predict_proba(data)
 
