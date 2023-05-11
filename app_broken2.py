c5208eb2d0dba8d3810dd9b4b8e7ff7401f39c3e
S19925 <80154425+s19925@users.noreply.github.com>
2023-05-11 21:38:55 +0200

    Add files via upload

--- app_broken2.py
+++ app_broken2.py
@@ -11,7 +11,6 @@ startTime = datetime.now()
 # import znanych nam bibliotek
 
 data = pd.read_csv("DSP_13.csv", delimiter=';')
-print(data.columns)  # Print the column names to check for discrepancies
 # Extract the features and target variable
 features = data[['objawy', 'wiek', 'choroby_wsp', 'wzrost', 'leki']]
 target = data['zdrowie']
@@ -45,8 +44,34 @@ def main():
 	with overview:
 		st.title("Titanic App")
 
+	with right:
+		objawy_slider = st.slider("Objawy", value=1, min_value=0, max_value=10)
+		wiek_slider = st.slider("Wiek", value=1, min_value=0, max_value=120)
+		choroby_wsp_slider = st.slider("Choroby współistniejące", value=1, min_value=0, max_value=5)
+		wzrost_slider = st.slider("Wzrost", value=1, min_value=120, max_value=220)
+		leki_slider = st.slider("Leki", value=1, min_value=0, max_value=10)
+
+	# read the data
+	df = pd.read_csv('DSP_13.CSV')
+	# remove the missing values
+	df.dropna(inplace=True)
+	# take the median of each column
+	medians = df.median()
+	# replace the missing values with the medians
+	objawy_slider = medians['objawy'] if objawy_slider is None else objawy_slider
+	wiek_slider = medians['wiek'] if wiek_slider is None else wiek_slider
+	choroby_wsp_slider = medians['choroby_wsp'] if choroby_wsp_slider is None else choroby_wsp_slider
+	wzrost_slider = medians['wzrost'] if wzrost_slider is None else wzrost_slider
+	leki_slider = medians['leki'] if leki_slider is None else leki_slider
+
+	data = [[objawy_slider, wiek_slider, choroby_wsp_slider, wzrost_slider, leki_slider]]
 	survival = model.predict(data)
 	s_confidence = model.predict_proba(data)
+
+	with prediction:
+		st.subheader("Czy taka osoba przeżyłaby katastrofę?")
+		st.subheader(("Tak" if survival[0] == 1 else "Nie"))
+		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 	with prediction:
 		st.subheader("Czy taka osoba przeżyłaby katastrofę?")
